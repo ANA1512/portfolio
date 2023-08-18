df3c28c3330ab1d3adb897647a7e606d7dfbd0de
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-08-18 11:53:45 +0200

    Add files via upload

--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,7 @@
 const styles = {
-    paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-16 py-10",
-
-  
-  
+    paddingX: "sm:px-14 px-4",
+    paddingY: "sm:py-14 py-4",
+    padding: "sm:px-14 px-4 sm:py-14 py-10",
   };
   
   export { styles };
